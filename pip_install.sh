68e06d41ad15b796a0b9cc0ee01847f21600dcad
Yew Mun <yewmun.yip@crick.ac.uk>
2024-08-12 18:16:19 +0100

    add a bash script to install the pip packages sequentially

---
+++ pip_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121 &&
+pip install torch-cluster -f https://data.pyg.org/whl/torch-2.1.0+cu121.html &&
+pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.1.0+cu121.html &&
+pip install torch-spline-conv -f https://data.pyg.org/whl/torch-2.1.0+cu121.html &&
+pip install -r requirements.txt &&
+pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable"
