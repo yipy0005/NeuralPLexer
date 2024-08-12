bf2b529eab4f598a95bda0af32e0607606d005bb
Yew Mun <yewmun.yip@crick.ac.uk>
2024-08-12 18:40:28 +0100

    edit the pip_install.sh to load the required GCC module

--- pip_install.sh
+++ pip_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ml ATK/2.38.0-GCCcore-11.3.0
+
 pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121 &&
 pip install torch-cluster -f https://data.pyg.org/whl/torch-2.1.0+cu121.html &&
 pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.1.0+cu121.html &&
