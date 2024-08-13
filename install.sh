aafbce4d5dc9e68591b293b7a582b0f6744ecbee
Yew Mun <yewmun.yip@crick.ac.uk>
2024-08-13 09:58:33 +0100

    add a bash script to automate the installation of libraries and packages

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Clone the NeuralPLexer repository from GitHub
+git clone git@github.com:yipy0005/NeuralPLexer.git
+
+# Change directory to the cloned repository
+cd NeuralPLexer
+
+# Download the required models and datasets
+wget -O neuralplexermodels_downstream_datasets_predictions.zip https://zenodo.org/records/10373581/files/neuralplexermodels_downstream_datasets_predictions.zip?download=1
+
+# Create and set up the required environment using the Makefile
+make environment
+
+# Activate the conda environment created by the Makefile
+conda activate neuralplexer_dev
+
+# Load the necessary module (ATK)
+ml ATK/2.38.0-GCCcore-11.3.0
+
+# Run the pip_install.sh script to install additional dependencies
+sh pip_install.sh
+
+# Change directory to the openfold subdirectory
+cd openfold
+
+# Install the openfold package
+python setup.py install
+
+# Go back to the NeuralPLexer root directory
+cd ..
+
+# Install the NeuralPLexer package in editable mode
+pip install -e .
