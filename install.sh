e62ee9a1daf5cd4eaa67c80e00172b842e889b94
Yew Mun <yewmun.yip@crick.ac.uk>
2024-08-13 10:36:33 +0100

    add a bash script to automate the installation of libraries and packages

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Load the necessary module (ATK)
+ml ATK/2.38.0-GCCcore-11.3.0
+
 # Download the required models and datasets
 wget -O neuralplexermodels_downstream_datasets_predictions.zip https://zenodo.org/records/10373581/files/neuralplexermodels_downstream_datasets_predictions.zip?download=1
+
+# Unzip the downloaded models and datasets
+unzip neuralplexermodels_downstream_datasets_predictions.zip
 
 # Create and set up the required environment using the Makefile
 make environment
 
 # Activate the conda environment created by the Makefile
 conda activate neuralplexer_dev
-
-# Load the necessary module (ATK)
-ml ATK/2.38.0-GCCcore-11.3.0
 
 # Run the pip_install.sh script to install additional dependencies
 sh pip_install.sh
